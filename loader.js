484de5d3b78a0acb314ac22fce1289df82febb97
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-02-23 15:09:57 +0100

    the loader no longer crashes if the szn-select is already loaded

--- loader.js
+++ loader.js
@@ -5,6 +5,9 @@
   // %{EMBEDDABLE_LOADER}%
 
   const script = makeSznSelectBundleScript()
+  if (!script) {
+    return
+  }
 
   // TODO: do not use document.write if the hosts do not match: https://www.chromestatus.com/feature/5718547946799104
   if (['interactive', 'complete'].indexOf(document.readyState) === -1) {
