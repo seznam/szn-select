54ebb11e07306962c433a8d02b68ca5c088f0101
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-05-09 15:06:48 +0200

    #8 Firefox/macOS a11y improvements: selecting options by typing their text values

--- a11y/VanillaAriaLabelledBySelect.js
+++ a11y/VanillaAriaLabelledBySelect.js
@@ -1,6 +1,7 @@
 /* global AccessibilityBroker, SznElements */
 
 class VanillaAriaLabelledBySelect extends AccessibilityBroker {
+
   constructor(select, ui, sznSelect) {
     super(select, ui, sznSelect)
 
@@ -15,6 +16,8 @@ class VanillaAriaLabelledBySelect extends AccessibilityBroker {
     })
     this._blurTimeout = null
     this._lastOptionId = 0
+    this._typedText = ''
+    this._typingTimeout = null
 
     this._onFocus = this.onFocus.bind(this)
     this._onBlur = this.onBlur.bind(this)
@@ -59,6 +62,11 @@ class VanillaAriaLabelledBySelect extends AccessibilityBroker {
 
     this._removeEventListeners()
     this._selectObserver.disconnect()
+
+    if (this._typingTimeout) {
+      clearTimeout(this._typingTimeout)
+      this._typingTimeout = null
+    }
   }
 
   onUiClicked(event) {
@@ -187,6 +195,40 @@ class VanillaAriaLabelledBySelect extends AccessibilityBroker {
         break
       default:
         break // nothing to do
+    }
+
+    if (!event.altKey && !event.ctrlKey && !event.metaKey && event.key.length === 1) {
+      if (this._typingTimeout) {
+        clearTimeout(this._typingTimeout)
+      }
+
+      this._typedText += event.key.toLowerCase()
+      for (const option of enabledOptions) {
+        if (option.text.toLowerCase().substring(0, this._typedText.length) === this._typedText) {
+          newlySelectedOption = option
+          break
+        }
+      }
+      if (
+        (!newlySelectedOption || newlySelectedOption === enabledOptions[selectedIndex]) &&
+        / Firefox\//.test(navigator.userAgent)
+      ) {
+        // Unfortunately, Firefox does not provide us with accented characters in keyboard events when the character is
+        // typed using dead keys.
+        const transliteratedTypedText = VanillaAriaLabelledBySelect.transliterate(this._typedText)
+        for (const option of enabledOptions) {
+          const transliteratedText = VanillaAriaLabelledBySelect.transliterate(option.text).toLowerCase()
+          if (transliteratedText.substring(0, transliteratedTypedText.length) === transliteratedTypedText) {
+            newlySelectedOption = option
+            break
+          }
+        }
+      }
+
+      this._typingTimeout = setTimeout(() => {
+        this._typingTimeout = null
+        this._typedText = ''
+      }, VanillaAriaLabelledBySelect.TYPING_TIMEOUT)
     }
 
     if (newlySelectedOption && newlySelectedOption !== enabledOptions[selectedIndex]) {
@@ -353,3 +395,236 @@ VanillaAriaLabelledBySelect.compatibilityTest = select => (
 )
 
 VanillaAriaLabelledBySelect._lastId = 0
+
+VanillaAriaLabelledBySelect.TYPING_TIMEOUT = 500 // milliseconds
+
+// The following code is derived from the node-transliterator npm package:
+// https://github.com/psyduk/node-transliterator
+VanillaAriaLabelledBySelect.transliterate = string => {
+  return string.split('').map(character => {
+    if (
+      character.charCodeAt(0) > 127 &&
+      typeof VanillaAriaLabelledBySelect.TRANSLITERATION_TABLE[character] !== 'undefined'
+    ) {
+      return VanillaAriaLabelledBySelect.TRANSLITERATION_TABLE[character]
+    } else {
+      return character
+    }
+  }).join('')
+}
+VanillaAriaLabelledBySelect.TRANSLITERATION_TABLE = {
+  ä: 'ae',
+  æ: 'ae',
+  ǽ: 'ae',
+  ö: 'oe',
+  œ: 'oe',
+  ü: 'ue',
+  Ä: 'Ae',
+  Ü: 'Ue',
+  Ö: 'Oe',
+  À: 'A',
+  Á: 'A',
+  Â: 'A',
+  Ã: 'A',
+  Å: 'A',
+  Ǻ: 'A',
+  Ā: 'A',
+  Ă: 'A',
+  Ą: 'A',
+  Ǎ: 'A',
+  à: 'a',
+  á: 'a',
+  â: 'a',
+  ã: 'a',
+  å: 'a',
+  ǻ: 'a',
+  ā: 'a',
+  ă: 'a',
+  ą: 'a',
+  ǎ: 'a',
+  ª: 'a',
+  Ç: 'C',
+  Ć: 'C',
+  Ĉ: 'C',
+  Ċ: 'C',
+  Č: 'C',
+  ç: 'c',
+  ć: 'c',
+  ĉ: 'c',
+  ċ: 'c',
+  č: 'c',
+  Ð: 'D',
+  Ď: 'D',
+  Đ: 'D',
+  ð: 'd',
+  ď: 'd',
+  đ: 'd',
+  È: 'E',
+  É: 'E',
+  Ê: 'E',
+  Ë: 'E',
+  Ē: 'E',
+  Ĕ: 'E',
+  Ė: 'E',
+  Ę: 'E',
+  Ě: 'E',
+  è: 'e',
+  é: 'e',
+  ê: 'e',
+  ë: 'e',
+  ē: 'e',
+  ĕ: 'e',
+  ė: 'e',
+  ę: 'e',
+  ě: 'e',
+  Ĝ: 'G',
+  Ğ: 'G',
+  Ġ: 'G',
+  Ģ: 'G',
+  ĝ: 'g',
+  ğ: 'g',
+  ġ: 'g',
+  ģ: 'g',
+  Ĥ: 'H',
+  Ħ: 'H',
+  ĥ: 'h',
+  ħ: 'h',
+  Ì: 'I',
+  Í: 'I',
+  Î: 'I',
+  Ï: 'I',
+  Ĩ: 'I',
+  Ī: 'I',
+  Ĭ: 'I',
+  Ǐ: 'I',
+  Į: 'I',
+  İ: 'I',
+  ì: 'i',
+  í: 'i',
+  î: 'i',
+  ï: 'i',
+  ĩ: 'i',
+  ī: 'i',
+  ĭ: 'i',
+  ǐ: 'i',
+  į: 'i',
+  ı: 'i',
+  Ĵ: 'J',
+  ĵ: 'j',
+  Ķ: 'K',
+  ķ: 'k',
+  Ĺ: 'L',
+  Ļ: 'L',
+  Ľ: 'L',
+  Ŀ: 'L',
+  Ł: 'L',
+  ĺ: 'l',
+  ļ: 'l',
+  ľ: 'l',
+  ŀ: 'l',
+  ł: 'l',
+  Ñ: 'N',
+  Ń: 'N',
+  Ņ: 'N',
+  Ň: 'N',
+  ñ: 'n',
+  ń: 'n',
+  ņ: 'n',
+  ň: 'n',
+  ŉ: 'n',
+  Ò: 'O',
+  Ó: 'O',
+  Ô: 'O',
+  Õ: 'O',
+  Ō: 'O',
+  Ŏ: 'O',
+  Ǒ: 'O',
+  Ő: 'O',
+  Ơ: 'O',
+  Ø: 'O',
+  Ǿ: 'O',
+  ò: 'o',
+  ó: 'o',
+  ô: 'o',
+  õ: 'o',
+  ō: 'o',
+  ŏ: 'o',
+  ǒ: 'o',
+  ő: 'o',
+  ơ: 'o',
+  ø: 'o',
+  ǿ: 'o',
+  º: 'o',
+  Ŕ: 'R',
+  Ŗ: 'R',
+  Ř: 'R',
+  ŕ: 'r',
+  ŗ: 'r',
+  ř: 'r',
+  Ś: 'S',
+  Ŝ: 'S',
+  Ş: 'S',
+  Š: 'S',
+  ś: 's',
+  ŝ: 's',
+  ş: 's',
+  š: 's',
+  ſ: 's',
+  Ţ: 'T',
+  Ť: 'T',
+  Ŧ: 'T',
+  ţ: 't',
+  ť: 't',
+  ŧ: 't',
+  Ù: 'U',
+  Ú: 'U',
+  Û: 'U',
+  Ũ: 'U',
+  Ū: 'U',
+  Ŭ: 'U',
+  Ů: 'U',
+  Ű: 'U',
+  Ų: 'U',
+  Ư: 'U',
+  Ǔ: 'U',
+  Ǖ: 'U',
+  Ǘ: 'U',
+  Ǚ: 'U',
+  Ǜ: 'U',
+  ù: 'u',
+  ú: 'u',
+  û: 'u',
+  ũ: 'u',
+  ū: 'u',
+  ŭ: 'u',
+  ů: 'u',
+  ű: 'u',
+  ų: 'u',
+  ư: 'u',
+  ǔ: 'u',
+  ǖ: 'u',
+  ǘ: 'u',
+  ǚ: 'u',
+  ǜ: 'u',
+  Ý: 'Y',
+  Ÿ: 'Y',
+  Ŷ: 'Y',
+  ý: 'y',
+  ÿ: 'y',
+  ŷ: 'y',
+  Ŵ: 'W',
+  ŵ: 'w',
+  Ź: 'Z',
+  Ż: 'Z',
+  Ž: 'Z',
+  ź: 'z',
+  ż: 'z',
+  ž: 'z',
+  Æ: 'AE',
+  Ǽ: 'AE',
+  ß: 'ss',
+  Ĳ: 'IJ',
+  ĳ: 'ij',
+  Œ: 'OE',
+  ƒ: 'f',
+}
